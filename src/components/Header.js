57bf1b729865a4bc1cf4345682a7434840622c79
Ezequiel <ezequiel.lesca@gmail.com>
2023-08-28 11:25:00 -0300

    trabajando navBar y header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+// importamos el logo
+import Logo from "../assets/logo.svg"
+
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          {/* Logo */}
+          <a href='#'>
+            <img src={Logo} alt='' />
+          </a>
+            {/* aca va el boton */}
+            <button className='btn btn-sm'>Trabaja Conmigo</button>
+        </div>
+      </div>
+    </header>
+  )
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 // se importan los iconos de la biblioteca react-icons/bi
 
 import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { BsClipboardData, BsBriefcase, BsChatSquare, BsChatSquareText } from "react-icons/bs";
 
 // se importa link de la biblioteca react-scroll
 
@@ -11,44 +11,70 @@ import { Link } from 'react-scroll';
 
 const Nav = () => {
   return (
-  <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
 
-    <div className='container mx-auto'>
+      <div className='container mx-auto'>
 
-      {/* nav inner/  nav interno */}
-     
-      <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50'> 
+        {/* nav inner/  nav interno */}
 
-        <Link to='home' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          {/* este le da el icono casa */}
-          <BiHomeAlt />
-        </Link >
+        <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50'>
 
-        <Link to='about' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          {/* este le da el icono user */}
-          <BiUser />
-        </Link>
+          <Link
+            to='home'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            offset={-200}
+            className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            {/* este le da el icono casa */}
+            <BiHomeAlt />
+          </Link >
 
-        <Link to='services' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          {/* este le da el icono carpeta de clip */}
-          <BsClipboardData />
-        </Link >
+          <Link
+            to='about'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            {/* este le da el icono user */}
+            <BiUser />
+          </Link>
 
-        <Link to='work' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          {/* este le da el icono portafolio */}
-          <BsBriefcase />
-        </Link>
-        
-        <Link to='contact' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          {/* este le da el icono portafolio */}
-          <BsBriefcase />
-        </Link>
+          <Link
+            to='services'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            {/* este le da el icono carpeta de clip */}
+            <BsClipboardData />
+          </Link >
+
+          <Link
+            to='work'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            {/* este le da el icono portafolio */}
+            <BsBriefcase />
+          </Link>
+
+          <Link
+            to='contact'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            {/* este le da el icono portafolio */}
+            <BsChatSquareText />
+          </Link>
+
+        </div>
 
       </div>
 
-    </div>
-    
-  </nav>
+    </nav>
   );
 };
 
